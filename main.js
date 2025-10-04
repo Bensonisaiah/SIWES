700322fe1c1d05da2a94b2f230fe0332fe2a720a
Benson Isaiah <isaiahobenson@gmail.com>
2025-10-04 21:58:46 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// let fruits = ["orange", "apple", "banana", "coconut"]
+
+// for(let i=0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+
+// console.log(fruits.length)
+//  fruits.sort()
+
+//  for(let fruit of fruits){
+//      console.log(fruit)
+//  }
+
+// spread operator
+
+
+//  let num = [1, 2, 3, 4, 5, 6]
+//  console.log(Math.max(...num))
+//  console.log(Math.min(...num))
+
+//  let namee = "isaiah";
+//  let letters = [...namee].join("_");
+//  console.log(letters)
+
+// joining multiple arrays
+
+// let veg = ["onion", "cabbage", "carrot", "tullips"]
+// let newf = [...fruits, ...veg, "and others", ...veg].join("|");
+// console.log(newf);
+// console.log(fruits.join("|"));
